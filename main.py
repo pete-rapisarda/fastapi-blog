a4ec1a63041001cfb74917ae32d2390c529bbb10
Pete Rapisarda <pete@robopete.com>
2025-04-21 05:35:11 -0400

    first working blog MVP using fastapi

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+posts = [
+    {"id":1,"title":"Hello World","content":"This is your first blog post."},
+    {"id":2,"title":"FastAPI ROcks","content":"Building with FastAPI is fun and fast!"}
+]
+
+@app.get("/",response_class=HTMLResponse)
+def home(request:Request):
+    return templates.TemplateResponse("index.html",{"request":request,"posts":posts})
