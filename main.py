65f2a2905abb0570c9fc918001bbf5449a0d97bd
Pete Rapisarda <pete@robopete.com>
2025-04-23 06:04:08 -0400

    Updated get and post routes, imported Form and RedirectResponse modules, and added create.html

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -10,6 +10,15 @@ posts = [
     {"id":2,"title":"FastAPI Rocks","content":"Building with FastAPI is fun and fast!"}
 ]
 
-@app.get("/",response_class=HTMLResponse)
+@app.get("/create",response_class=HTMLResponse)
 def home(request:Request):
     return templates.TemplateResponse("index.html",{"request":request,"posts":posts})
+
+@app.post("/",response_class=HTMLResponse)
+def create_form(request: Request):
+    return templates.TemplateResponse("create.html",{"request":request})
+
+@app.post("/create")
+def create_post(request: Request,title: str = Form(...),content:str=Form(...)):
+    posts.append({"id":len(posts) + 1, "title":title, "content":content})
+    return RedirectResponse("/",status_code="303")
