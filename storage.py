33e14f96ea15126ae18bf8e1033a816a4fac85c8
Pete Rapisarda <pete@robopete.com>
2025-04-27 07:05:40 -0400

    updated storage.py to included deleted path to deleted posts json and included two new functions to load and save deleted posts (archive) (#9)

--- storage.py
+++ storage.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 DATA_FILE = Path("posts.json")
+DELETED_FILE = Path("deleted_posts.json")
 
 def load_posts():
     if DATA_FILE.exists():
@@ -11,4 +12,14 @@ def load_posts():
 
 def save_posts(posts):
     with open(DATA_FILE,"w") as f:
+        json.dump(posts, f, indent=2)
+
+def load_deleted_posts():
+    if DELETED_FILE.exists():
+        with open(DELETED_FILE,"r") as f:
+            return json.load(f)
+        return []
+
+def save_deleted_posts(posts):
+    with open(DELETED_FILE, "w") as f:
         json.dump(posts, f, indent=2)
