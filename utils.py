7a0bcf07bf3f8bff5bcc0c89e856cb7185842e82
Pete Rapisarda <pete@robopete.com>
2025-05-06 05:42:49 -0400

    created a login check in utils #22

--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from fastapi.responses import RedirectResponse
 
 templates = Jinja2Templates(directory="templates")
 
 def set_flash(request:Request, message:str) -> None:
     request.session["flash"] = message
+
+def require_login(request:Request):
+    if "username" not in request.session:
+        return RedirectResponse(
+            f"/login?error=unauthorized",status_code=303
+        )
